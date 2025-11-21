978acbd980d12fa278b4f8ff63e082e081562120
Alexander Langrock <arock1216@gmail.com>
2025-11-20 22:27:12 -0500

    Menus and audio boilerplate

---
+++ scripts/volume_slider.gd
@@ -0,0 +1,24 @@
+extends Control
+
+var master_index= AudioServer.get_bus_index("Master")
+var sfx_index= AudioServer.get_bus_index("SFX")
+var music_index= AudioServer.get_bus_index("Music")
+
+func _ready():
+	$MasterVolume.value = AudioServer.get_bus_volume_linear(master_index)
+	$MusicVolume.value = AudioServer.get_bus_volume_linear(music_index)
+	$SFXVolume.value = AudioServer.get_bus_volume_linear(sfx_index)
+
+
+func _on_master_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(master_index, value)
+
+
+func _on_music_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(music_index, value)
+
+
+func _on_sfx_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(sfx_index, value)
+	
+	
