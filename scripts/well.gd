12dd9d16986205a37324b902f9d067556e5c17db
loudsunrises <nicole.cerundolo@gmail.com>
2025-12-02 17:54:18 -0500

    Nicole Update
    Updated tiles in main, deleted & re-added many trees (they're still broken), updated Water Shrine

--- scripts/well.gd
+++ scripts/well.gd
@@ -1,29 +1,29 @@
-extends Sprite2D
+extends AnimatedSprite2D
 class_name Well
 
-const MAX_THRESHOLD : int = 4
-const WIDTH : float = 640.0
-const HEIGHT : float = 320.0
+const MAX_HP : int = 4
 const MAX_HITS : int = 1
 
 signal all_done
 
-@onready var increment : float = 640.0
-@onready var threshold : int = 0
 @onready var hits = 0
 
+func ready():
+	self.play("default")
+	
 func increment_well():
 	hits += 1
-	if hits < MAX_HITS:
-		return
-	
-	if threshold < MAX_THRESHOLD:
-		hits = 0
-		threshold = clampi(threshold + 1, 0, MAX_THRESHOLD)
-		var current_atlas_texture : AtlasTexture = self.texture
-		current_atlas_texture.region = Rect2(increment * threshold, 0.0, WIDTH, HEIGHT)
-		if threshold == MAX_THRESHOLD:
-			all_done.emit()
+	match hits:
+			1:
+				self.play("Lvl1")
+			2:
+				self.play("Lvl2")
+			3:
+				self.play("Lvl3")
+			4:
+				self.play("Lvl4")
+				all_done.emit()
+			_: pass
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area is Bullet:
